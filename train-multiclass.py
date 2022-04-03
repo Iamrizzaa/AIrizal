f6e209f4c3621be983acc5fedd65999df7bdd1c9
Iamrizzaa <102036298+Iamrizzaa@users.noreply.github.com>
2022-04-04 01:49:36 +0800

    Update train-multiclass.py

--- train-multiclass.py
+++ train-multiclass.py
@@ -2,10 +2,10 @@
 import sys
 import os
 import numpy as np
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Activation, Dropout, Flatten, Dense
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.layers import Activation, Dropout, Flatten, Dense
 from keras import backend as K
 
 train_data_dir = './data/train'
