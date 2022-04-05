7f05135c29180d2a98a6b0fe554e2c6f2ea2201f
Iamrizzaa <102036298+Iamrizzaa@users.noreply.github.com>
2022-04-06 02:40:01 +0800

    Update train-multiclass.py

--- train-multiclass.py
+++ train-multiclass.py
@@ -2,7 +2,7 @@
 import sys
 import os
 import numpy as np
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
@@ -14,11 +14,11 @@ validation_data_dir = './data/validation'
 """
 Parameters
 """
-img_width, img_height = 150, 150
-nb_train_samples = 150
+img_width, img_height = 150,150
+nb_train_samples = 350 
 nb_validation_samples = 100
-epochs = 10
-batch_size = 64
+epochs = 150
+batch_size = 32
 
 if K.image_data_format() == 'channels_first':
     input_shape = (3, img_width, img_height)
@@ -39,11 +39,9 @@ model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-
 model.add(Flatten())
-model.add(Dense(150))
+
 model.add(Activation("relu"))
-model.add(Dropout(0.5))
 model.add(Dense(36, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy',
@@ -51,9 +49,13 @@ model.compile(loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 train_datagen = ImageDataGenerator(
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
     rescale=1. / 255,
     shear_range=0.2,
     zoom_range=0.2,
+    brightness_range = (0.5, 1.5),
     horizontal_flip=True)
 
 test_datagen = ImageDataGenerator(rescale=1. / 255)
@@ -77,11 +79,36 @@ hist = model.fit_generator(
     epochs=epochs,
     validation_data = validation_generator,
     validation_steps = nb_validation_samples // batch_size)
+    
 
 target_dir = './models/'
 if not os.path.exists(target_dir):
   os.mkdir(target_dir)
 model.save('./models/model.h5')
 model.save_weights('./models/weights.h5')
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+plt.plot (hist.history['loss'], color = 'blue', label = 'train')
+plt.plot (hist.history['val_loss'], color = 'orange', label = 'train')
+plt.grid(True)
+plt.title("Train & test loss with epochs\n", fontsize = 16)
+plt.xlabel ("Training epochs", fontsize=12)
+plt.ylabel ("Train & test loss", fontsize=12)
+plt.show()
+
+plt.plot (hist.history['acc'], color = 'blue', label = 'train')
+plt.plot (hist.history['val_acc'], color = 'orange', label = 'train')
+plt.grid(True)
+plt.title("Train & test accuracy with epochs\n", fontsize=16)
+plt.xlabel ("Training epochs", fontsize=12)
+plt.ylabel ("Train & test loss", fontsize=12)
+plt.show()
+
+
 
 
